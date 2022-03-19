9d6ac047f1f979a1da0940522e186905c52bf895
omarbahaa7 <89842435+omarbahaa7@users.noreply.github.com>
2022-03-19 16:13:15 +0200

    Create modulo.java

---
+++ modulo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class modulo {
+    String watermelon(int x) {
+        if ((x > 2) && (x % 2 == 0)) {
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+    }
+}
